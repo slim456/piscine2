4d1e74ca3890173950f97e5b614e50c1a7b4f559
Mehdi SLIMANI <mslimani@e2r12p21.42.fr>
2019-05-23 14:17:51 +0200

    ee

---
+++ ex03/House.class.php
@@ -0,0 +1,17 @@
+<?php
+	 abstract class House
+	 {
+	 	abstract function getHouseName();
+	 	abstract function getHouseMotto();
+	 	abstract function getHouseSeat();
+	 	function introduce()
+	 	{
+	 		print("House ");
+	 		print($this->getHouseName());
+	 		print(" of ");
+	 		print($this->getHouseSeat());
+	 		print(" : ");
+	 		print('"' . $this->getHouseMotto() . '"'  . "\n");
+	 	}
+	 }
+?>
